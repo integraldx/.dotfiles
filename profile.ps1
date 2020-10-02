63e51fbb8a69e7f38f4527f32d4fbde0d540b44f
integraldx <integraldx@integraldx.me>
2020-10-02 23:33:05 +0900

    Abbreviated git alias script importation

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,7 @@
-
 Import-Module oh-my-posh
 
 Set-Theme agnoster
 
 Set-Alias e explorer -Force
 
-Invoke-Expression -Command (Get-Content -Path (Join-Path $PSScriptRoot .\git-alias.ps1) | Out-String)
+. $PSScriptRoot/git-alias.ps1
