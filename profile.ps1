65a823513f1a9788986d721514ca425c9fbd688d
integraldx <integraldx@integraldx.me>
2022-06-29 23:58:22 +0900

    Add fsmonitor and z.lua

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,5 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\robbyrussel.omp.json | Invoke-Expression
+Invoke-Expression (& { (lua "$HOME\Appdata\Local\z\z.lua" --init powershell) -join "`n" })
 
 Set-Alias vim nvim
 Set-Alias e explorer -Force
