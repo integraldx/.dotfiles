62ba8b064b4d2473fbf77b50d25aadcb384c0558
integraldx <integraldx@integraldx.me>
2024-08-06 19:39:05 +0900

    Remove z.lua initialization script

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,4 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\robbyrussell.omp.json | Invoke-Expression
-Invoke-Expression (& { (lua "$HOME\Appdata\Local\z\z.lua" --init powershell) -join "`n" })
 
 Set-Alias vim nvim
 Set-Alias e explorer -Force
